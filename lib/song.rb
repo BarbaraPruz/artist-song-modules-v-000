b99d48b4a3d5acce8e6c57aefd0e6c199dc6cc2d
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-05 21:38:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
-  extend Paramable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -26,9 +26,9 @@ class Song
 #    @@songs.clear
 #  end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#  def to_param
+#    name.downcase.gsub(' ', '-')
+#  end
 
 #  def self.count
 #    self.all.count
